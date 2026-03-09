d4eab7fcd509385a10502293815a8a1360ed2a95
yazhinivivekanandh <yazhini.vivekanandh2025@bitsdesign.edu.in>
2026-03-09 15:40:17 +0530

    Update Week9-time.time().py

--- Week9-time.time().py
+++ Week9-time.time().py
@@ -100,13 +100,19 @@ import time
 
 IN1 = Pin(13, Pin.OUT)
 IN2 = Pin(12, Pin.OUT)
+ENA = PWM(Pin(15))
+ENA.freq(1000)
 
-while True:
+ while True:
+    ENA.duty(1000)
     IN1.value(1)
     IN2.value(0)
     time.sleep(3)
+    ENA.duty(1000)
     IN1.value(0)
     IN2.value(1)
     time.sleep(3)
+  
+7.
 
 
