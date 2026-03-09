67f3adaa01024d3929e88179c7dd41d221ff6734
naishark06 <naishark06@gmail.com>
2026-03-09 10:50:34 +0530

    Update Week9-Time.py

--- Week9-Time.py
+++ Week9-Time.py
@@ -33,4 +33,36 @@
   print (t2)
   T = t2-t1
   print(T)
-39. 
+39. Calculate time difference between a for loop and without a for loop
+  import time
+  print("This is the time using a for loop")
+  t1 = time.ticks_ms()
+  print(t1)
+  for a in range (0,10,1):
+      print("Naisha")
+  t2 = time.ticks_ms()
+  print(t2)
+  T1 = time.ticks_diff(t2,t1)
+  print(T1, "miliseconds. With a for loop")
+  
+  print("This is the time without for loop")
+  t3 = time.ticks_ms()
+  print(t3)
+  
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  print("Naisha")
+  
+  t4 = time.ticks_ms()
+  print(t4)
+  T2 = time.ticks_diff(t4, t3)
+  print (T2, "miliseconds. Without a for loop")
+  Difference = time.ticks_diff(T2, T1)
+  print (Difference, "miliseconds. The total difference with a for loop and without a for loop")
